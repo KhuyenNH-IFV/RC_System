fd1a0256446d6edde1558935d9d6ce3a614c5b88
huukhuyen <nguyenhuukhuyenudn@gmail.com>
2017-08-28 08:11:10 +0700

    create project RC_System

---
+++ block/header.tpl
@@ -0,0 +1,79 @@
+<header class="header is_mb">
+    <div class="wrapheaderMb">
+        <div class="logo">
+            <a href="index.html">
+                <img src="images/nav_01.png" alt="">
+            </a>
+        </div>
+        <div class="toggle">
+            <img src="images/toggle.png" alt="">
+        </div>
+    </div>
+    <nav class="navHeader">
+        <ul>
+            <li><a href="index.html">トップページ</a></li>
+            <li><a href="about.html">会社案内</a></li>
+            <li><a href="product.html">製品案内</a></li>
+			<li><a href="service.html">事業案内</a></li>
+			<li><a href="recruit.html">採用案内</a></li>
+            <li><a href="contact.html">お問合わせ</a></li>
+            <!-- <li class="social">
+                <a href=""><img src="images/nav_06_mb.png" alt=""></a>
+                <a href=""><img src="images/nav_07_mb.png" alt=""></a>
+            </li> -->
+        </ul>
+    </nav>
+</header>
+<!-- /header -->
+<aside class="l-wrapper">
+    <div class="gnav">
+        <h1 class="logoNavLeft"><a href="index.html"><img src="images/nav_01.png" alt=""></a></h1>
+        <nav class="gnav-options">
+            <div class="gnav-body">
+                <ul class="gnav-primary-options">
+                    <li class="gnav-options_items">
+                        <a href="about.html"><img src="images/nav_02.png" alt=""></a>
+                        <!--  -->
+                        <div class="gnav-secondary-options">
+                            <ul>
+                                <li><a href="about.html">ご挨拶</a></li>
+                                <li><a href="about2.html">会社概要</a></li>
+                                <li><a href="about3.html">アクセス</a></li>
+								<li><a href="about4.html">LASの会</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="gnav-options_items">
+                        <a href="product.html"><img src="images/nav_03.png" alt=""></a>
+                        <div class="gnav-secondary-options">
+                            <ul>
+                                <li><a href="product.html">製品案内トップ</a></li>
+								<li><a href="product2.html">新聞社向け</a></li>
+                                <li><a href="product3.html">折込チラシ業向け</a></li>
+                                <li><a href="product4.html">その他管理システム</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="gnav-options_items">
+                        <a href="#"><img src="images/nav_04.png" alt=""></a>                        
+                    </li>
+                    <li class="gnav-options_items">
+                        <a href="#"><img src="images/nav_05.png" alt=""></a>                        
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- /.gnav-options-->
+        <nav class="gnav-links">
+            <ul>
+                <li class="gnav-facebook">
+                    <a href="contact.html"><img src="images/nav_06.png" alt=""></a>
+                </li>
+                <li class="gnav-map">
+                    <a href="about3.html#a01"><img src="images/nav_07.png" alt=""></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
+<!-- Grand Menu -->
---
+++ js/main.js
@@ -0,0 +1,142 @@
+var fadeTopEffect = {
+    setInit: function() {
+        $(window).scroll(function() {
+            $(".fadeTop").each(function() {
+                var elemPos = $(this).offset().top;
+                var scroll = $(window).scrollTop();
+                var windowHeight = $(window).height();
+                if (scroll > elemPos - windowHeight + 100) {
+                    $(this).addClass('ef-slide');
+                }
+            });
+        }).trigger("scroll");
+    },
+}
+
+$(document).ready(function() {
+    // Check version IE
+    if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
+        $('body').append("<link rel='stylesheet' type='text/css' href='css/ieOnly.css' />");
+    }
+
+    // Slider-in menu left
+    $('.gnav').css("left", "0");
+
+    // Fade title slider
+    $('.sectionSlider .titleSection').css({
+        "opacity": "1",
+        "top": "253px"
+    });
+
+    fadeTopEffect.setInit();
+    // Show five news first : PC
+    $('.contentSection .itemTopic:gt(4)').hide();
+
+    // Show three news first : Mobile
+    var widthBr = $(window).width();
+    var heightBr = $(window).height();
+    if (widthBr <= 768) {
+        $('.contentSection .itemTopic:gt(2)').hide();
+        $(window).scroll(function(event) {
+            if ($(this).scrollTop() > indexListMenu - 62) {
+                $('.sectionThumbnail').addClass('fixed').next().css({
+                    'padding-top': hThumb + pdtNext,
+                    'transition-duration': "0ms"
+                });
+            } else {
+                $('.sectionThumbnail').removeClass('fixed').next().css('padding-top', pdtNext);
+            }
+        });
+        var headMb_height = $('.wrapheaderMb').outerHeight(true);
+        var heightSlider = parseInt(heightBr - headMb_height - 65);
+        $('.sectionSlider').css('height', heightSlider);
+        $('.m-slider').css('height', heightSlider);
+        $('.m-slider .image').css('height', heightSlider);
+        $('.slick-list.draggable').css('height', heightSlider);
+
+    }
+
+    // Toggle nav mobile
+    $('.wrapheaderMb .toggle').click(function(event) {
+        $('.navHeader').fadeToggle('fast');
+        // Toggle img
+        var urlI = $(this).find('img');
+        let openNav = "images/toggle.png";
+        let closeNav = "images/toggle_close.png";
+        if (urlI.attr('src') === openNav) {
+            $('.toggle img').attr('src', closeNav);
+        } else {
+            $('.toggle img').attr('src', openNav);
+        }
+    });
+
+    // Hover images
+
+    // Fixed menu list
+    try {
+        var indexListMenu = $('.sectionThumbnail').offset().top;
+        var hThumb = $('.sectionThumbnail').outerHeight(true);
+        var pdtNext = parseInt($('.sectionThumbnail').next().css('padding-top').split("px"));
+        $(window).scroll(function(event) {
+            if ($(this).scrollTop() > indexListMenu) {
+                $('.sectionThumbnail').addClass('fixed').next().css({
+                    'padding-top': hThumb + pdtNext,
+                    'transition-duration': "0ms"
+                });
+            } else {
+                $('.sectionThumbnail').removeClass('fixed').next().css('padding-top', pdtNext);
+            }
+        });
+    } catch (err) {}
+
+    // Hover listProduct
+    $('.listNews .item').hover(function() {
+        var url = $(this).find('.btnNewsM');
+        $(this).find('.btnNewsM').attr('src', $(this).find('.btnNewsM').attr('src').replace('-ovoff', '-ovon'));
+    }, function() {
+        $(this).find('.btnNewsM').attr('src', $(this).find('.btnNewsM').attr('src').replace('-ovon', '-ovoff'));
+    });
+
+    // Set height for menu listTop mobile
+    equalheight = function(container) {
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+        $(container).each(function() {
+
+            $el = $(this);
+            $($el).height('auto')
+            topPostion = $el.position().top;
+
+            if (currentRowStart != topPostion) {
+                for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0; // empty the array
+                currentRowStart = topPostion;
+                currentTallest = $el.height();
+                rowDivs.push($el);
+            } else {
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            }
+            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        });
+    }
+    $(window).load(function() {
+        equalheight('.sectionThumbnail .items');
+        equalheight('.row2 .name');
+        equalheight('.row3 .note');
+        equalheight('.row4 .note');
+    });
+    $(window).resize(function() {
+        equalheight('.sectionThumbnail .items');
+        equalheight('.row2 .name');
+        equalheight('.row3 .note');
+        equalheight('.row4 .note');
+    });
+});
